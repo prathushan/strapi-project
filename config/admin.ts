b55ca3eb8bef54e092438741c50c7a23b233c4b6
np-usha <prathusha.n22@gmail.com>
2025-02-07 19:11:34 +0530

    Fix: Added all keys

--- config/admin.ts
+++ config/admin.ts
@@ -1,17 +1,19 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET','xKk5USTobBtlXhCvvXI7MA=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','MCL3km0iE+xQp/JLF6ovMQ=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT','defaultTransferSalt'),
     },
   },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
+  
+flags: {
+  nps: env.bool('FLAG_NPS', true),
+  promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+},
+
 });
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
     'users-permissions': {
       config: {
-        jwtSecret: env('JWT_SECRET', 'yourGeneratedSecret'),
+        jwtSecret: env('JWT_SECRET','jXY7m9XqB5ATBHRcxyClkg=='),
       },
     },
   });
